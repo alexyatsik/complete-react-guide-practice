bcd3f77fc2df3ef1c62c843afd3203c49a5c5232
Alexeii <alexeii.yatsenko@gmail.com>
2022-09-03 23:23:48 +0300

    Add modal component basement

---
+++ src/Modal/Modal.js
@@ -0,0 +1,13 @@
+import {Card} from "../UI/Card";
+
+import styles from './Modal.module.css';
+
+function Modal() {
+  return (
+    <Card>
+      <h1>Modal window component</h1>
+    </Card>
+  );
+}
+
+export {Modal};
