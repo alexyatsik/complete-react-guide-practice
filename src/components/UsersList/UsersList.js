a6fd5218c005bfd24242221bbfdd9ff3da3ea7f4
Alexeii Yatsenko <alexeii.yatsenko@gmail.com>
2022-09-04 23:22:00 +0300

    Fix empty ul visualisation

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -4,18 +4,22 @@ import {Card} from "../UI/Card";
 import styles from './UsersList.module.css';
 
 function UsersList(props) {
-  return (
-    <Card>
-      <ul className={styles.list}>
-          {props.usersCatalogue.map(user =>
-              <User
-                name={user.name}
-                age={user.age}
-              />
-          )}
-      </ul>
-    </Card>
-  );
+    if (props.usersCatalogue.length === 0) {
+        return <ul />;
+    }
+
+    return (
+        <Card>
+          <ul className={styles.list}>
+              {props.usersCatalogue.map(user =>
+                  <User
+                    name={user.name}
+                    age={user.age}
+                  />
+              )}
+          </ul>
+        </Card>
+    );
 }
 
 export {UsersList};
